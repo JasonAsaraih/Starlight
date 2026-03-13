319dd248f62457433978dbc0ebb2e82e18df8342
Jason Asariah <jasonsean.asariah@calbaptist.edu>
2026-03-13 13:22:59 -0700

    initial sfml

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Game");
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Game");
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-            if (event.type == sf::Event::Closed)
+        while (auto event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
                 window.close();
+        }
 
         window.clear();
         window.display();
